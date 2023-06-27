ab43b00a4bced349f2fdb4e02594d24e36128325
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2023-06-27 11:44:25 +0330

    Revert "Revert "add auth component and its styles""
    This reverts commit f98f76d0acd97e7c09063d65b9b7ffa820cc163b.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Ingredients from "./Components/Ingredients/Ingredients/Ingredients";
 import "./App.css";
+import Auth from "./Components/Auth/Auth";
 function App() {
-  return <Ingredients />;
+  // return <Ingredients />;
+  return <Auth />;
 }
 
 export default App;
